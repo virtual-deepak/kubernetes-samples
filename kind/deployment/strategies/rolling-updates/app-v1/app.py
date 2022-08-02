d72add511bed4e4d79c14d46b9f242db339a4a5c
Deepak Agarwal <deepak.agarwal@icertis.com>
2022-08-02 21:10:41 +0530

    Rolling updates with python app

---
+++ kind/deployment/strategies/rolling-updates/app-v1/app.py
@@ -0,0 +1,16 @@
+import os
+import socket
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World running from v1: ' + socket.gethostname()
+
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=PORT)
